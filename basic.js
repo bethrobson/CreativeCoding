d2a163bfb60a6127877b7ca820c2bd90931b8fa3
Elisabeth Robson <bethrobson@gmail.com>
2023-09-07 09:47:45 -0700

    Update basic.js
    updating variable

--- basic.js
+++ basic.js
@@ -1,14 +1,15 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background(255);
-	rectMode(CENTER);
-	noFill();
-	
-	let squareSize = 40;
-	let numSquares = 110;
-	for (let i = 0; i < numSquares; i++) {
-		let x = random(100, width-100);
-		let y = random(100, height-100);
-		rect(x, y, squareSize, squareSize);
-	}
+    rectMode(CENTER);
+    noFill();
+
+    let squareSize = 40;
+    let numSquares = 110;
+    let margin = 100;
+    for (let i = 0; i < numSquares; i++) {
+        let x = random(margin, width-margin);
+        let y = random(margin, height-margin);
+        rect(x, y, squareSize, squareSize);
+    }
 }
